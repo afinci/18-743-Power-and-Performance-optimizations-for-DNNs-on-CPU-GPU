97f812f20a837ab804b5482a72305c05711fd1e9
Rudy Chin <bb1168kk@gmail.com>
2017-12-05 12:27:31 -0500

    script

--- iterate.sh
+++ iterate.sh
@@ -2,6 +2,6 @@ for net in ResNet-152 googlenet alexnet
 do
     for cpu in consumer automotive office
     do
-        python eval.py logs/pwr_tmp_${net}-deploy_${cpu}_998_1734.log
+        python eval.py logs/pwr_tmp_${net}-deploy_${cpu}_$1_$2.log
     done
 done
